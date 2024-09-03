963a7676b43979529a235d78d60a34f994d8f61b
Michael Aronian <aronian.m@gmail.com>
2024-09-02 22:45:16 -0400

    Initial Commit

---
+++ Shart-IOS/Shart_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shart_IOSApp.swift
+//  Shart-IOS
+//
+//  Created by Michael Aronian Aronian on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Shart_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
